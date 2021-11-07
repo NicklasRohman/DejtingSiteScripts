1b11e859f9ac364bea9cd60e2cb6f9daac1ebdf4
NicklasRohman <nicklasrohman@gmail.com>
2021-11-07 23:46:25 +0100

    working without loop

---
+++ Match/BasedOnYourRecentLikes.js
@@ -0,0 +1,42 @@
+let minAge = 18;
+let maxAge = 37;
+let numberOfProfiles = 10;
+
+let ageCity = document.getElementsByClassName("profile-infos__age-city")[0].textContent;
+let age = ageCity.substring(0,2);
+
+if(age < minAge && age > maxAge) {
+document.getElementsByClassName("shuffle-card__interaction shuffle-card__interaction--no")[0].click();
+}
+else {
+
+var element = document.getElementsByClassName("IconChip__StyledText-sc-lwjjbr-2 cmsPci");
+
+let onlyFirstProfile = 0;
+const profileValues = [];
+
+for(let i = 0; i < element.length; i++) {
+
+    if(element[i].textContent.includes(" km")) {
+        onlyFirstProfile++;
+        }
+
+    if(onlyFirstProfile > 1) {
+    break;
+    }
+        profileValues[i] = element[i].textContent;
+}
+
+    for(let i = 0; i < profileValues.length; i++) {
+
+    if ((profileValues[i].includes("Har barn"))|| (profileValues[i].includes("Kanske")) || (profileValues[i].includes("Vill inte ha barn"))) {
+            console.log("No button");
+            document.getElementsByClassName("shuffle-card__interaction shuffle-card__interaction--no")[0].click();
+            break;
+        } else {
+            nextProfile = false;
+            console.log("This might interest you");
+        }
+    }
+   }
+
