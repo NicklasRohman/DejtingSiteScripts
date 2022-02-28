7bc6ecb87fde2316161c97778a75221b8ab5f0ef
NicklasRohman <nicklasrohman@gmail.com>
2022-02-28 23:56:07 +0100

    remove big loop

--- Match/BasedOnYourRecentLikes.js
+++ Match/BasedOnYourRecentLikes.js
@@ -1,56 +1,51 @@
 let minAge = parseInt(18);
 let maxAge = parseInt(38);
-let bigLoop;
-let breakBigLoop = 0;
 
-for( bigLoop=0; bigLoop<3; bigLoop++) {
-let ageCity = document.getElementsByClassName("profile-infos__age-city")[0].textContent;
-let age = parseInt(ageCity.substring(0,2));
 
-if(age < minAge && age > maxAge) {
-document.getElementsByClassName("shuffle-card__interaction shuffle-card__interaction--no")[0].click();
-}
-else {
-var element = document.getElementsByClassName("ProfileFullDealbreakers__DealbreakersList-sc-l04px0-0");
 
-let onlyFirstProfile = 0;
-const profileValues = [];
+    let ageCity = document.getElementsByClassName("profile-infos__age-city")[i].textContent;
+    let age = parseInt(ageCity.substring(0,2));
 
-for(let elementLoop = 0; elementLoop < element.length; elementLoop++) {
+    if(age < minAge && age > maxAge)
+    {
+        document.getElementsByClassName("shuffle-card__interaction shuffle-card__interaction--no")[0].click();
+    }
+    else
+    {
+        var element = document.getElementsByClassName("ProfileFullDealbreakers__DealbreakersList-sc-l04px0-0");
 
-    if(element[elementLoop].textContent.includes(" km")) {
-        onlyFirstProfile++;
+        let onlyFirstProfile = 0;
+        const profileValues = [];
+
+        for(let elementLoop = 0; elementLoop < element.length; elementLoop++)
+        {
+            if(element[elementLoop].textContent.includes(" km"))
+            {
+                onlyFirstProfile++;
+            }
+
+            if(onlyFirstProfile > 1)
+            {
+                break;
+            }
+
+            profileValues[elementLoop] = element[elementLoop].textContent;
         }
 
-    if(onlyFirstProfile > 1) {
-            break;
-    }
-        profileValues[elementLoop] = element[elementLoop].textContent;
-}
-
-    for(let profileLoop = 0; profileLoop < profileValues.length; profileLoop++) {
-
-    if ( profileValues[profileLoop].includes("Röker regelbundet")   ||
-      profileValues[profileLoop].includes("Kanske")                 ||
-      profileValues[profileLoop].includes("Vill inte ha barn") ) {
-                document.getElementsByClassName("shuffle-card__interaction shuffle-card__interaction--no")[0].click();
+        for(let profileLoop = 0; profileLoop < profileValues.length; profileLoop++)
+        {
+            if ( profileValues[profileLoop].includes("Röker regelbundet")   ||
+                profileValues[profileLoop].includes("Kanske")               ||
+                profileValues[profileLoop].includes("Vill inte ha barn")    ||
+                profileValues[profileLoop].includes("Har barn")
+                )
+                {
+                    document.getElementsByClassName("shuffle-card__interaction shuffle-card__interaction--no")[0].click();
+                }
+            else
+                {
+                    console.log("kan vara intressant");
+                    document.getElementsByClassName("shuffle-card__interaction shuffle-card__interaction--yes")[0].click();
+                }
             }
-    else if (profileValues[profileLoop].includes("Har barn")) {
-            if(profileValues[profileLoop].includes("Vill inte ha barn")) {
-                console.log("kan vara intressant men har barn");
-            } else {
-            document.getElementsByClassName("shuffle-card__interaction shuffle-card__interaction--no")[0].click();
-
-            }
-             }
-    else {
-        breakBigLoop = 1;
-        console.log("kan vara intressant");
-        break;
-    }
-    }
-   }
-   if (breakBigLoop > 0){
-   break;
-   }
-}
+        }
